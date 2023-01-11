4ffef9c673be24a2a148f6a5d4e92c38d5cbc8e3
daluga0503 <daniel.luquegallardo@gmail.com>
2023-01-11 23:36:29 +0100

    fraccion added

---
+++ ejercicio4/Fraccion.java
@@ -0,0 +1,82 @@
+package java_capitulo_9.ejercicio4;
+
+public class Fraccion implements Cloneable{
+    private int numerador;
+    private int denominador;
+
+    public int getNumerador(){
+        return this.numerador;
+    }
+
+    public int getDenominador(){
+        return this.denominador;
+    }
+
+    private Fraccion(){
+        this.numerador = 0;
+        this.denominador = 1;
+    }
+
+    public Fraccion(int num, int den){
+        this();
+        if(den==0)
+            System.out.println("Fracción no permitida");
+        else{
+            this.numerador = num;
+            this.denominador = den;
+        }
+    }
+
+    public Fraccion(Fraccion f){
+        this(f.getNumerador(), f.getDenominador());
+    }
+
+    @Override
+    public Fraccion clone(){
+        return new Fraccion(this.getNumerador(), this.getDenominador());
+    }
+
+    public Fraccion invierte(){
+        return new Fraccion(this.denominador, this.numerador);
+    }
+
+    public Fraccion multiplica(Fraccion f){
+        int numerador = this.numerador * f.getNumerador();
+        int denominador = this.denominador * f.getDenominador();
+        return  new Fraccion(numerador, denominador);
+    }
+
+    public Fraccion multiplica(int n){
+        return this.multiplica(new Fraccion(n,1));        
+    }   
+
+    public Fraccion divide(Fraccion f){
+        return this.multiplica(f.invierte());
+    }
+
+    public Fraccion divide(int n){
+        return this.divide(new Fraccion(n,1));
+    }
+
+    public Fraccion simplifica(){
+        int num = this.numerador;
+        int den = this.denominador;
+        int menor = Math.min(num, den);
+        for(int i = 2; i <= Math.abs(menor); i++){
+            if(num%Math.abs(i)==0 && den%Math.abs(i)==0){
+                num/=i;
+                den/=i;
+                menor = Math.min(num, den);
+            }
+        }
+        return new Fraccion(num, den);
+    }
+
+    @Override
+    public String toString(){
+
+        boolean negativo = (this.numerador < 0 || this.denominador < 0) && !(this.numerador < 0 && this.denominador < 0);
+        return (negativo?"-":"")+Math.abs(this.numerador)+"/"+Math.abs(this.denominador); 
+    }
+
+}
